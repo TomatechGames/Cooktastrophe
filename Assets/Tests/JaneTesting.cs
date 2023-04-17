a8b0469f7c341eb8c2beb49b3c169f50e23a84f3
janekeyes <janekeyes123@gmail.com>
2023-04-17 12:25:16 +0100

    Testing

---
+++ Assets/Tests/JaneTesting.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class JaneTesting
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void JaneTestingSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator JaneTestingWithEnumeratorPasses()
+    {
+        SceneManager.LoadScene(1);
+        var components = GenerateComponents();
+        //do things with the components
+        yield return null;
+    }
+
+    (TableAppliance. GrabItemCompenent, GameStateManager.CustomerGroup)GenerateComponents()
+    {
+        var group = new GameStateManager.CustomerGroup();
+        return (group, Item2);
+    }
+} g
