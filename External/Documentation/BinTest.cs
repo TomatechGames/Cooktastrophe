d5fc99119e912ce5e523298241ac7c9b58d96b4c
AishaNtl <aishantuli003@gmail.com>
2023-04-22 13:18:56 +0100

    aishatest

---
+++ External/Documentation/BinTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BinTest
+{
+	public BinTest()
+	{
+	}
+}
---
+++ External/Documentation/FridgeApplianceTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FridgeApplianceTest
+{
+
+    public FridgeApplianceTest()
+	{
+	}
+}
